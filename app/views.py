518490cc9eb514a8582f323f3de0ef5eedee46d3
joe <jvd5839@rit.edu>
2016-02-22 17:59:27 -0500

    Moved to a different OAuth lib that plays nicely with python3.5 and implemented logging in/logging out via facebook

--- app/views.py
+++ app/views.py
@@ -1,49 +1,68 @@
 __author__ = 'SWEN356 Team 4'
 
 from app import app
-from flask_oauth import OAuth
-from flask import render_template, redirect, url_for, session, request
+from flask_oauthlib.client import OAuth
+from flask import render_template, redirect, url_for, session, request, flash, jsonify
 
-oauth = OAuth()
+
+"""
+Defines the OAuth object needed for logging in via Facebook/Google (NYI)
+"""
+oauth = OAuth(app)
 facebook = oauth.remote_app('facebook',
-    base_url='https://localhost:5000',
-    request_token_url=None,
-    access_token_url='/oauth/access_token',
-    authorize_url='https://www.facebook.com/dialog/oauth',
-    consumer_key='966689803422128',
-    consumer_secret='5ebcacfed9b216675ed00ff074d87c4b',
-    request_token_params={'scope':'email'},
-)
+                            base_url='https://graph.facebook.com/',
+                            request_token_url=None,
+                            access_token_url='/oauth/access_token',
+                            authorize_url='https://www.facebook.com/dialog/oauth',
+                            consumer_key='966689803422128',
+                            consumer_secret='5ebcacfed9b216675ed00ff074d87c4b',
+                            request_token_params={'scope' : 'email'},
+                            )
 
-
+"""
+Gets the current facebook user token, if there is one.
+"""
 @facebook.tokengetter
 def get_fb_token(token=None):
     return session.get('facebook_token')
 
-
+"""
+Handles the oauth response object sent back from Facebook
+resp - a dict containing user id and the access token
+"""
 @app.route('/oauth-authorized')
-@facebook.authorized_handler
-def oauth_authorized(resp):
-    next_url = facebook.request.args.get('next') or url_for('index', _external=True)
+def oauth_authorized():
+    resp = facebook.authorized_response()
+    next_url = request.args.get('next') or url_for('index', _external=True)
     if resp is None:
-        facebook.flash('Your sign in request was denied.')
+        flash('Your sign in request was denied.')
         return redirect(next_url)
+    session['facebook_token'] = (resp['access_token'], '')
+    user = facebook.get("/me").data
+    session['name'] = user['name']
+    flash('You were signed in as %s' % session['name'])
+    return redirect(url_for('home'))
 
-    session['facebook_token'] = (resp['oauth_token'], resp['oauth_token_secret'])
-    session['facebook_user'] = resp['email']
-    facebook.flash('You were signed in as %s' % resp['email'])
-    return redirect(next_url)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    if session:
+        return redirect(url_for('home'))
     return render_template('index.html')
 
 
 @app.route('/login')
 def login():
-    return facebook.authorize(callback=url_for('index', _external=True))
+    return facebook.authorize(callback=url_for('oauth_authorized', _external=True))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 
-
+@app.route('/home')
+def home():
+    return render_template('home.html')
